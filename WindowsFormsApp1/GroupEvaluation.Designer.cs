47ee594db053aef03d30da742d2d5ef3d1d09a95
Anmol Tariq <anmoltariqbutt@gmail.com>
2019-04-04 03:12:17 +0500

    mid

--- WindowsFormsApp1/GroupEvaluation.Designer.cs
+++ WindowsFormsApp1/GroupEvaluation.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-	partial class AssignAdvisor
+	partial class GroupEvaluation
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -29,15 +29,15 @@
 		private void InitializeComponent()
 		{
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.comboBox2 = new System.Windows.Forms.ComboBox();
-			this.comboBox1 = new System.Windows.Forms.ComboBox();
 			this.label3 = new System.Windows.Forms.Label();
-			this.advisor = new System.Windows.Forms.TextBox();
 			this.label8 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label1 = new System.Windows.Forms.Label();
+			this.obtMarks = new System.Windows.Forms.TextBox();
 			this.Save = new System.Windows.Forms.Button();
 			this.View = new System.Windows.Forms.Button();
+			this.groupCombo = new System.Windows.Forms.ComboBox();
+			this.evaluationCombo = new System.Windows.Forms.ComboBox();
 			this.dataGridView1 = new System.Windows.Forms.DataGridView();
 			this.groupBox1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -46,67 +46,40 @@
 			// groupBox1
 			// 
 			this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-			this.groupBox1.Controls.Add(this.comboBox2);
-			this.groupBox1.Controls.Add(this.comboBox1);
+			this.groupBox1.Controls.Add(this.evaluationCombo);
+			this.groupBox1.Controls.Add(this.groupCombo);
+			this.groupBox1.Controls.Add(this.obtMarks);
 			this.groupBox1.Controls.Add(this.label3);
-			this.groupBox1.Controls.Add(this.advisor);
 			this.groupBox1.Controls.Add(this.label8);
 			this.groupBox1.Controls.Add(this.label2);
 			this.groupBox1.Controls.Add(this.label1);
-			this.groupBox1.Location = new System.Drawing.Point(3, 1);
+			this.groupBox1.Location = new System.Drawing.Point(-6, 1);
 			this.groupBox1.Name = "groupBox1";
 			this.groupBox1.Size = new System.Drawing.Size(794, 259);
-			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabIndex = 2;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
-			// 
-			// comboBox2
-			// 
-			this.comboBox2.FormattingEnabled = true;
-			this.comboBox2.Location = new System.Drawing.Point(207, 201);
-			this.comboBox2.Name = "comboBox2";
-			this.comboBox2.Size = new System.Drawing.Size(193, 21);
-			this.comboBox2.TabIndex = 24;
-			this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
-			// 
-			// comboBox1
-			// 
-			this.comboBox1.FormattingEnabled = true;
-			this.comboBox1.Items.AddRange(new object[] {
-            "Main Advisor",
-            "Co-Advisror",
-            "Industry Advisor"});
-			this.comboBox1.Location = new System.Drawing.Point(207, 141);
-			this.comboBox1.Name = "comboBox1";
-			this.comboBox1.Size = new System.Drawing.Size(193, 21);
-			this.comboBox1.TabIndex = 23;
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
 			this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(70, 200);
+			this.label3.Location = new System.Drawing.Point(67, 166);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(96, 22);
+			this.label3.Size = new System.Drawing.Size(120, 22);
 			this.label3.TabIndex = 21;
-			this.label3.Text = "Project Title";
-			// 
-			// advisor
-			// 
-			this.advisor.Location = new System.Drawing.Point(207, 71);
-			this.advisor.Name = "advisor";
-			this.advisor.Size = new System.Drawing.Size(193, 20);
-			this.advisor.TabIndex = 9;
+			this.label3.Text = "Obtained Marks";
 			// 
 			// label8
 			// 
 			this.label8.AutoSize = true;
 			this.label8.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label8.Location = new System.Drawing.Point(67, 138);
+			this.label8.Location = new System.Drawing.Point(67, 118);
 			this.label8.Name = "label8";
-			this.label8.Size = new System.Drawing.Size(98, 22);
+			this.label8.Size = new System.Drawing.Size(88, 22);
 			this.label8.TabIndex = 7;
-			this.label8.Text = "Advisor Role";
+			this.label8.Text = "Evaluation";
+			this.label8.Click += new System.EventHandler(this.label8_Click);
 			// 
 			// label2
 			// 
@@ -114,9 +87,9 @@
 			this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label2.Location = new System.Drawing.Point(67, 68);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(82, 22);
+			this.label2.Size = new System.Drawing.Size(53, 22);
 			this.label2.TabIndex = 1;
-			this.label2.Text = "Advisor Id";
+			this.label2.Text = "Group";
 			// 
 			// label1
 			// 
@@ -124,17 +97,24 @@
 			this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label1.Location = new System.Drawing.Point(304, 16);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(96, 36);
+			this.label1.Size = new System.Drawing.Size(201, 36);
 			this.label1.TabIndex = 0;
-			this.label1.Text = "Advisor";
+			this.label1.Text = "Group Evaluation";
+			// 
+			// obtMarks
+			// 
+			this.obtMarks.Location = new System.Drawing.Point(297, 166);
+			this.obtMarks.Name = "obtMarks";
+			this.obtMarks.Size = new System.Drawing.Size(193, 20);
+			this.obtMarks.TabIndex = 24;
 			// 
 			// Save
 			// 
 			this.Save.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.Save.Location = new System.Drawing.Point(74, 266);
+			this.Save.Location = new System.Drawing.Point(39, 266);
 			this.Save.Name = "Save";
 			this.Save.Size = new System.Drawing.Size(75, 23);
-			this.Save.TabIndex = 8;
+			this.Save.TabIndex = 26;
 			this.Save.Text = "Save";
 			this.Save.UseVisualStyleBackColor = true;
 			this.Save.Click += new System.EventHandler(this.Save_Click);
@@ -142,33 +122,54 @@
 			// View
 			// 
 			this.View.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.View.Location = new System.Drawing.Point(594, 266);
+			this.View.Location = new System.Drawing.Point(671, 266);
 			this.View.Name = "View";
 			this.View.Size = new System.Drawing.Size(75, 23);
-			this.View.TabIndex = 13;
+			this.View.TabIndex = 27;
 			this.View.Text = "View";
 			this.View.UseVisualStyleBackColor = true;
+			this.View.Click += new System.EventHandler(this.View_Click);
+			// 
+			// groupCombo
+			// 
+			this.groupCombo.FormattingEnabled = true;
+			this.groupCombo.Location = new System.Drawing.Point(297, 71);
+			this.groupCombo.Name = "groupCombo";
+			this.groupCombo.Size = new System.Drawing.Size(193, 21);
+			this.groupCombo.TabIndex = 25;
+			// 
+			// evaluationCombo
+			// 
+			this.evaluationCombo.FormattingEnabled = true;
+			this.evaluationCombo.Location = new System.Drawing.Point(297, 119);
+			this.evaluationCombo.Name = "evaluationCombo";
+			this.evaluationCombo.Size = new System.Drawing.Size(193, 21);
+			this.evaluationCombo.TabIndex = 26;
 			// 
 			// dataGridView1
 			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
 			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Location = new System.Drawing.Point(1, 295);
+			this.dataGridView1.Location = new System.Drawing.Point(-6, 295);
 			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.Size = new System.Drawing.Size(796, 172);
-			this.dataGridView1.TabIndex = 24;
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+			this.dataGridView1.Size = new System.Drawing.Size(794, 158);
+			this.dataGridView1.TabIndex = 28;
 			// 
-			// AssignAdvisor
+			// GroupEvaluation
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(797, 467);
+			this.ClientSize = new System.Drawing.Size(788, 450);
 			this.Controls.Add(this.dataGridView1);
 			this.Controls.Add(this.View);
 			this.Controls.Add(this.Save);
 			this.Controls.Add(this.groupBox1);
-			this.Name = "AssignAdvisor";
-			this.Text = "AssignAdvisor";
-			this.Load += new System.EventHandler(this.AssignAdvisor_Load);
+			this.Name = "GroupEvaluation";
+			this.Text = "GroupEvaluation";
+			this.Load += new System.EventHandler(this.GroupEvaluation_Load);
 			this.groupBox1.ResumeLayout(false);
 			this.groupBox1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -179,15 +180,15 @@
 		#endregion
 
 		private System.Windows.Forms.GroupBox groupBox1;
-		private System.Windows.Forms.TextBox advisor;
+		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label8;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox obtMarks;
 		private System.Windows.Forms.Button Save;
 		private System.Windows.Forms.Button View;
-		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.ComboBox comboBox1;
-		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.ComboBox evaluationCombo;
+		private System.Windows.Forms.ComboBox groupCombo;
 		private System.Windows.Forms.DataGridView dataGridView1;
 	}
 }
---
+++ WindowsFormsApp1/GroupProject.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class GroupProject : Form
+	{
+		public GroupProject()
+		{
+			InitializeComponent();
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+
+		SqlConnection con = new SqlConnection(@"Data Source=PC\SQLEXPRESS;Initial Catalog=ProjectA;MultipleActiveResultSets=true;User ID=sa;Password=Anmoltariqbutt");
+
+
+		private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Save_Click(object sender, EventArgs e)
+		{
+
+			if (groupCombo.Text != "" && projectCombo.Text != "")
+			{
+				con.Open();
+
+
+				string query = "INSERT into GroupProject(ProjectId, GroupId, AssignmentDate) VALUES ( (SELECT Id FROM Project WHERE Project.Title = '" + projectCombo.Text + "' ), (Select Id FROM [Group] WHERE [Group].Id = '" + groupCombo.Text + "'), '" + DateTime.Now + "')";
+
+				SqlCommand cmd = new SqlCommand(query, con);
+
+
+
+
+				cmd.ExecuteNonQuery();
+				con.Close();
+				MessageBox.Show("Data saved Successfully", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+			}
+			else
+				MessageBox.Show("Fill all the Fields", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+		}
+
+		private void GroupProject_Load(object sender, EventArgs e)
+		{
+			SqlDataAdapter SDA = new SqlDataAdapter("Select * FROM [Group]", con);
+			DataTable dt = new DataTable();
+			SDA.Fill(dt);
+			for (int i = 0; i < dt.Rows.Count; i++)
+			{
+				groupCombo.Items.Add(dt.Rows[i]["Id"]);
+			}
+
+			SqlDataAdapter SDA1 = new SqlDataAdapter("Select * FROM Project", con);
+			DataTable dt1 = new DataTable();
+			SDA1.Fill(dt1);
+			for (int i = 0; i < dt1.Rows.Count; i++)
+			{
+				projectCombo.Items.Add(dt1.Rows[i]["Title"]);
+			}
+
+		}
+
+		private void View_Click(object sender, EventArgs e)
+		{
+			con.Open();
+
+			String query = "SELECT * FROM GroupProject ";
+			SqlDataAdapter SDA = new SqlDataAdapter(query, con);
+			DataTable dt = new DataTable();
+			SDA.Fill(dt);
+			dataGridView1.DataSource = dt;
+			con.Close();
+		}
+	}
+}
